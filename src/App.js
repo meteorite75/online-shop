593121745ccc64bf2d66171beb02f28173e946eb
shahab <razavi.shahab@yahoo.com>
2024-02-17 18:32:24 +0330

    parrent Component & Child Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
 
+// parrent Component 
 function App() {
-  const name = "shahab";
-  let age = 18;
   return (
-    <div className="App">
-      Hello {name}
-      You are now {age} years old
+    <div>
+      <User name='shahab' age={27}></User>
     </div>
   );
 }
+
+// Child Component
+const User = (prop) => {
+  return (
+    <div>
+      <h1>{prop.name}</h1>
+      <h2>{prop.age}</h2>
+    </div>
+  );
+};
 
 export default App;
